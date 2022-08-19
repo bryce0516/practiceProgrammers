f54290451065f9478ec1235d3442545d7ae0cf53
Hyesung Park <51251258+bryce0516@users.noreply.github.com>
2022-08-19 17:56:05 +0900

    update

--- src/test1/src/app.api.js
+++ src/test1/src/app.api.js
@@ -10,29 +10,48 @@ export default class Api {
     return result;
   };
 
-  static onClick = async (id, data, targetDom) => {
+  static onClick = async (dom, data, targetDom, stack) => {
+    const { dataset } = dom;
+    const { id } = dataset;
+    console.log("onClick inputed", id, data, targetDom, stack);
     if (targetDom) {
-      for (const chlid of targetDom.children) {
+      for (const chlid of document.querySelectorAll(".Node")) {
         targetDom.removeChild(chlid);
       }
     }
 
     let result = [];
     const target = data.filter((element, index) => element.id === id);
-    if (target && target[0].type === "DIRECTORY") {
-      result = await instance({
-        url: "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev",
-        addurl: `/${id}`,
-        method: "GET",
-        data: {},
-      });
+
+    console.log(target);
+    if (target.length !== 0) {
+      if (target[0].type === "DIRECTORY") {
+        result = {
+          data: await instance({
+            url: "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev",
+            addurl: `/${id}`,
+            method: "GET",
+            data: {},
+          }),
+          clickedName: target[0].name,
+        };
+      }
+      if (Object.keys(stack).length === 1) {
+        stack[target[0].name] = result.data;
+      }
     }
+
     return result;
     // console.log('onClick',super().state)
   };
 
-  static makeNode(arr) {
+  static makeNode(arr, stack) {
     let html = "";
+    if (Object.keys(stack).length > 1) {
+      html += `<div class="Node">
+            <img src="./assets/prev.png">
+          </div>`;
+    }
     if (arr && arr.length !== 0) {
       arr.map((element) => {
         if (element.type === "DIRECTORY") {
@@ -40,6 +59,11 @@ export default class Api {
                     <img src="./assets/directory.png">
                     <div>${element.name}</div>
                     </div>`;
+        } else if (element.type === "FILE") {
+          html += `<div class="Node" data-node-id="${element.id}">
+            <img src="./assets/file.png">
+            <div>${element.name}</div>
+            </div>`;
         }
       });
     }
--- src/test1/src/app.js
+++ src/test1/src/app.js
@@ -2,7 +2,8 @@ import Api from "./app.api.js";
 export default class App {
   state = {
     data: [],
-    stack: [],
+    stack: { root: [] },
+    isRoot: "root",
     html: "",
   };
   constructor(props) {
@@ -12,26 +13,31 @@ export default class App {
     this.init();
   }
 
-  setState({ key, value }, { key2, html }) {
+  setState({ key, value }, { key2, html }, clickedName) {
     console.log("setState", { key, value }, { key2, html });
+
+    if (this.state.isRoot === "root") {
+      this.state.stack["root"] = value;
+    }
+
     this.state[key] = value;
+    console.log(">>>>>>>>>> state", this.state);
     this.render(key2, html);
   }
 
   async init() {
     const result = await Api.rootFetch();
-    this.state.data = result;
     if (result) {
       this.setState(
         { key: "data", value: result },
-        { key2: "Nodes", html: Api.makeNode(result) }
+        { key2: "Nodes", html: Api.makeNode(result, this.state.stack) },
+        "root"
       );
     }
   }
 
   render(key, value) {
     let dom = null;
-    console.log(typeof value);
     if (key === "root") {
       dom = this.root;
     } else if (key === "Nodes") {
@@ -52,11 +58,17 @@ export default class App {
       dom.addEventListener("click", async (event) => {
         if (event.target.closest(".Node")) {
           const { nodeId } = event.target.closest(".Node").dataset;
-          const result = await Api.onClick(nodeId, this.state.data, this.Nodes);
-          if (result) {
+          const { data, clickedName } = await Api.onClick(
+            event.target.closest(".Node"),
+            this.state.data,
+            this.Nodes,
+            this.state.stack
+          );
+          if (data) {
             this.setState(
-              { key: "data", value: result },
-              { key2: "Nodes", html: Api.makeNode(result) }
+              { key: "data", value: data },
+              { key2: "Nodes", html: Api.makeNode(data, this.state.stack) },
+              clickedName
             );
           }
         }
